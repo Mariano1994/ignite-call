8b9ad5baf3b4c071510776777adf3058da584952
Mariano Capiliku <marianocapiliku@gmail.com>
2025-01-03 09:46:27 +0100

    configured react-hook-form, validation and error handling on register-user-form

--- app/components/user-register-form.tsx
+++ app/components/user-register-form.tsx
@@ -3,21 +3,57 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ArrowRight } from "@phosphor-icons/react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const RegiterUserFromSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Usuario tem que ter no minimo 3 letras" })
+    .transform((username) => username.toLocaleLowerCase()),
+  userfullname: z
+    .string()
+    .min(5, { message: "Seu nome tem que ter no minimo 5 letras " })
+    .transform((fullname) => fullname.toLocaleLowerCase()),
+});
+
+type regiterUserFormData = z.infer<typeof RegiterUserFromSchema>;
 
 function UserRegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+    reset,
+  } = useForm<regiterUserFormData>({
+    resolver: zodResolver(RegiterUserFromSchema),
+  });
+
+  const handlerRegisterNewUser = (data: regiterUserFormData) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <Card className="bg-gray-700 border-none w-[425px] py-6 mt-6 rounded-sm">
       <CardContent>
-        <form>
+        <form onSubmit={handleSubmit(handlerRegisterNewUser)}>
           <Label htmlFor="username" className="text-white text-xs font-light">
-            Nome de usuãrio
+            Nome de usuário
           </Label>
           <Input
             prefix="cal.com/"
             id="username"
-            className="w-full bg-gray-900 text-white border-none mt-2 mb-4"
+            className="w-full bg-gray-900 text-white border-none mt-2 mb-7"
+            {...register("username")}
           />
+
+          {/* Error Message */}
+          <div className="text-xs text-red-400 -mt-3 mb-2 ">
+            {errors.username?.message}
+          </div>
           <Label
             htmlFor="userfullname"
             className="text-white text-xs font-light"
@@ -28,9 +64,18 @@ function UserRegisterForm() {
             prefix="cal.com/"
             id="userfullname"
             className="w-full bg-gray-900 text-white border-none mt-2 mb-4"
+            {...register("userfullname")}
           />
 
-          <Button className="bg-green-300 hover:bg-brightness-50 w-full">
+          {/* Error Message */}
+          <div className="text-xs text-red-400 -mt-3 mb-2 ">
+            {errors.userfullname?.message}
+          </div>
+
+          <Button
+            type="submit"
+            className="bg-green-300 hover:bg-brightness-50 mt-3 w-full"
+          >
             Próximo passso
             <ArrowRight />
           </Button>
